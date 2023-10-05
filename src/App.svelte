f424d61ec03a6f7a8301072ba571ef0a925c5567
Paul Adams <ghusiyy@gmail.com>
2023-10-04 23:08:30 -0700

    remove boilerplate code

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import AppGame from "./lib/AppGame.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Stellar Adventures</h1>
 
   <div class="card">
-    <Counter />
+    <AppGame />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
   <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
+    Check out <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer">guide</a
+    >, to learn how to play!
   </p>
 </main>
 
